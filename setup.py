1d7e00e6d14f31776f8c8819e30fdf9a65b1203b
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-10-07 21:44:00 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cppt",
+    version='0.1.0',
+    py_modules=['src.interface'],
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'cppt = src.interface:cli',
+        ],
+    },
+)
---
+++ src/app/__init__.py
@@ -0,0 +1,2 @@
+from .commands.create.manager import manage as create_manage
+from .commands.fetch.manager import manage as fetch_manage
