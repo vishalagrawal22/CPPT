ddf60d44ab3e116185e01dcd80fb2dff9a829de7
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-10-09 13:49:51 +0530

    Add run support for java and python

--- src/app/commands/run/manager.py
+++ src/app/commands/run/manager.py
@@ -1,70 +1,103 @@
 import sys
 import click
 import subprocess
+import re
 
 from pathlib import Path
 
 from ...internal.folder_manager import Task
 
-def get_command(language):
+# checks if file is empty (only contains white spaces)
+def is_file_empty(file_path):
+    content = open(file_path, 'r').read()
+    if re.search(r'^\s*$', content):
+        return True
+    return False
+
+
+def get_command(lang):
     pass
 
-def cpp_compile(source_code_path, exec_path, compilation_error_path):
-    subprocess_data = subprocess.run(["g++-11", source_code_path, "-o", exec_path], capture_output=True, text=True)
-    with open(compilation_error_path, "w") as file:
-        file.writelines(subprocess_data.stderr)
-    return subprocess_data.returncode
 
-# def java_compile(source_code_path, exec_path, compilation_error_path):
-#     subprocess_data = subprocess.run(["javac", exec_path], capture_output=True, text=True)
-#     with open(compilation_error_path, "w") as file:
-#         file.writelines(subprocess_data.stderr)
-#     return subprocess_data.returncode
+def write_to_file(file_path, text):
+    with open(file_path, "w") as file:
+        file.write(text + "\n")
 
-def py_compile():
-    # just an empty function to avoid errors
-    return 0
 
-def cpp_run(exec_path, in_path, std_error_path, std_output_path):
-    infile = open(in_path, 'r')
-    in_txt = "".join(infile.readlines())
-    subprocess_data = subprocess.run([f"./{exec_path}"], input=in_txt, encoding='ascii', capture_output=True, text=True)
-    with open(std_error_path, "w") as file:
-        file.writelines(subprocess_data.stderr)
+def read_from_file(file_path):
+    file = open(file_path, "r")
+    text = "".join(file.readlines())
+    file.close()
+    return text
 
-    with open(std_output_path, "w") as file:
-        file.writelines(subprocess_data.stdout)
 
-    return subprocess_data.returncode
+def cpp_compile(source_code_path, error_path):
+    file_name_without_extension = source_code_path.stem
+    exec_path = error_path.parent / file_name_without_extension
 
-def java_run(exec_path, in_path, std_error_path, std_output_path):
-    infile = open(in_path, 'r')
-    in_txt = "".join(infile.readlines())
-    subprocess_data = subprocess.run([f"java {exec_path}"], input=in_txt, encoding='ascii', capture_output=True, text=True)
-    with open(std_error_path, "w") as file:
-        file.writelines(subprocess_data.stderr)
+    compilation_data = subprocess.run(
+        ["g++-11", source_code_path, "-o", exec_path], capture_output=True, text=True)
+    write_to_file(error_path, compilation_data.stderr)
 
-    with open(std_output_path, "w") as file:
-        file.writelines(subprocess_data.stdout)
+    return [exec_path, compilation_data.returncode]
 
-    return subprocess_data.returncode
 
-def py_run():
-    pass
+def cpp_run(exec_path, input_path, output_path, error_path):
+    run_data = subprocess.run([exec_path.absolute()], input=read_from_file(
+        input_path), capture_output=True, text=True)
+    write_to_file(error_path, run_data.stderr)
+    write_to_file(output_path, run_data.stdout)
+    return run_data.returncode
+
+
+def java_compile(source_code_path, error_path):
+    file_name_without_extension = source_code_path.stem
+    last_run_path = error_path.parent
+    exec_path = last_run_path / file_name_without_extension
+
+    compilation_data = subprocess.run(
+        ["javac", source_code_path, "-d", last_run_path], capture_output=True, text=True)
+    write_to_file(error_path, compilation_data.stderr)
+
+    return [exec_path, compilation_data.returncode]
+
+
+def java_run(exec_path, input_path, output_path, error_path):
+    run_data = subprocess.run(["java", "-cp", exec_path.parent.absolute(), exec_path.stem], input=read_from_file(
+        input_path), capture_output=True, text=True)
+    write_to_file(error_path, run_data.stderr)
+    write_to_file(output_path, run_data.stdout)
+    return run_data.returncode
+
+
+# exec path incase of interpreted language is source code path
+def py_run(source_code_path, input_path, output_path, error_path):
+    run_data = subprocess.run(["python3", source_code_path], input=read_from_file(
+        input_path), capture_output=True, text=True)
+    write_to_file(error_path, run_data.stderr)
+    write_to_file(output_path, run_data.stdout)
+    return run_data.returncode
+
+
+is_interpreted = {
+    ".py": True,
+    ".java": False,
+    ".cpp": False,
+}
 
 run_func = {
-            ".py": py_run, 
-            ".java": java_run, 
-            ".cpp": cpp_run,
+    ".py": py_run,
+    ".java": java_run,
+    ".cpp": cpp_run,
 }
 
 compile_func = {
-            ".py": py_compile, 
-            ".java": java_compile, 
-            ".cpp": cpp_compile,
+    ".java": java_compile,
+    ".cpp": cpp_compile,
 }
 
-def print_file(file_path, error=False):
+
+def print_file(file_path, is_error=False):
     if not file_path.exists():
         click.secho(f"File at {file_path} does not exist", fg="red", err=True)
         sys.exit(1)
@@ -74,9 +107,8 @@ def print_file(file_path, error=False):
 
     with open(file_path, 'r') as file:
         for line in file.readlines():
-            click.secho(line, fg="red", err=error, nl=False)
+            click.secho(line, fg="red", err=is_error, nl=False)       
     click.echo("")
-        
 
 
 def judge(task, filename_without_extension, extension, base_folder):
@@ -91,14 +123,18 @@ def judge(task, filename_without_extension, extension, base_folder):
 
     tc_list.sort()
 
-    source_code_path = (base_folder / filename_without_extension).with_suffix(extension)
-    exec_path = base_folder / f"{filename_without_extension}"
+    source_code_path = (
+        base_folder / filename_without_extension).with_suffix(extension)
     compilation_error_path = task.last_run_folder / "compilation_error.txt"
 
-    if compile_func[extension](source_code_path, exec_path, compilation_error_path) != 0:
-        click.secho(f"Compilation Error:\n", fg="red")
-        print_file(compilation_error_path, True)
-        sys.exit(1)
+    exec_path = Path()
+    if not is_interpreted[extension]:
+        exec_path, compile_returncode = compile_func[extension](
+            source_code_path, compilation_error_path)
+        if compile_returncode != 0:
+            click.secho(f"Compilation Error:\n", fg="red")
+            print_file(compilation_error_path, True)
+            sys.exit()
 
     for num in tc_list:
         in_path = task.tc_folder / f"in{num}.txt"
@@ -106,24 +142,32 @@ def judge(task, filename_without_extension, extension, base_folder):
         diff_path = task.last_run_folder / f"diff{num}.txt"
         std_output_path = task.last_run_folder / f"output{num}.txt"
         std_error_path = task.last_run_folder / f"error{num}.txt"
-        runtime_error_path = task.last_run_folder / f"runtime_error{num}.txt"
 
         click.secho(f"Running TC #{num}\n", fg="cyan")
 
-        if run_func[extension](exec_path, in_path, std_error_path, std_output_path) != 0:
+        run_returncode = 0
+        if is_interpreted[extension]:
+            run_returncode = run_func[extension](
+                source_code_path, in_path, std_output_path, std_error_path)
+        else:
+            run_returncode = run_func[extension](
+                exec_path, in_path, std_output_path, std_error_path)
+
+        if run_returncode != 0:
             click.secho(f"Rumtime Error #{num}\n", fg="red")
+            print_file(std_error_path, True)
             continue
-                
 
         diff_command = ["diff", "-b", "-B", std_output_path, ans_path]
-        run_diff_data = subprocess.run(diff_command, capture_output=True, text=True)
+        run_diff_data = subprocess.run(
+            diff_command, capture_output=True, text=True)
         with open(diff_path, "w") as file:
             file.writelines(run_diff_data.stdout)
 
-        if (diff_path.stat().st_size == 0):
+        if (is_file_empty(diff_path)):
             click.secho(f"Accepted #{num}\n", fg="green")
 
-            if std_error_path.stat().st_size != 0:
+            if not is_file_empty(std_error_path):
                 click.secho(f"Standard Error: ", fg="cyan")
                 print_file(std_error_path)
         else:
@@ -138,34 +182,35 @@ def judge(task, filename_without_extension, extension, base_folder):
             click.secho(f"Standard Output: ", fg="cyan")
             print_file(std_output_path)
 
-            if std_error_path.stat().st_size != 0:
+            if not is_file_empty(std_error_path):
                 click.secho(f"Standard Error: ", fg="cyan")
                 print_file(std_error_path)
+
 
 def manage(filename, base_folder):
     file_path = base_folder / filename
     extension = file_path.suffix
     filename_without_extension = file_path.stem
     if extension not in [".py", ".java", ".cpp"]:
-        click.secho(f"Language {file_path.suffix} is not supported (.py, .java, .cpp are only supported)", err=True, fg="red")
+        click.secho(
+            f"Language {file_path.suffix} is not supported (.py, .java, .cpp are only supported)", err=True, fg="red")
         sys.exit(1)
     else:
         task = Task(base_folder, filename_without_extension, extension)
         task_status = task.task_exists()
         if task_status != 3:
             if task_status == 0:
-                click.secho("Neither source code nor test data exists", err=True, fg="red")
+                click.secho(
+                    "Neither source code nor test data exists", err=True, fg="red")
             elif task_status == 1:
                 click.secho("Test Data does not exist", err=True, fg="red")
             elif task_status == 2:
                 click.secho("Source Code does not exist", err=True, fg="red")
-            
-            click.secho("Try overwriting existing files with create or fetch commands's --force option", err=True, fg="red")
-            click.secho("Caution: do not forget to copy any code in source file before using --force option", err=True, fg="red")
+
+            click.secho(
+                "Try overwriting existing files with create or fetch commands's --force option", err=True, fg="red")
+            click.secho(
+                "Caution: do not forget to copy any code in source file before using --force option", err=True, fg="red")
             sys.exit(1)
 
         judge(task, filename_without_extension, extension, base_folder)
-
-        
-
-        
