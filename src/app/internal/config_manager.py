dbb3f34dadfc09b652506c41fc9d4871b514268c
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-10-10 14:01:31 +0530

    Change create config implementation

--- src/app/internal/config_manager.py
+++ src/app/internal/config_manager.py
@@ -4,14 +4,19 @@ import yaml
 
 from .file_manager import read_from_file
 
-config_path = Path.home() / ".config/cppt/config.yaml"
+user_config_path = Path.home() / ".config/cppt/config.yaml"
 
 
 def create_config():
-    config_path.parent.mkdir(parents=True, exist_ok=True)
     default_config_path = Path(__file__).parent / "default_config.yaml"
-    config_path.write_bytes(default_config_path.read_bytes())
+    default_config = open(default_config_path, "r")
+    default_data = yaml.safe_load(default_config)
+
+    user_config_path.parent.mkdir(parents=True, exist_ok=True)
+    user_config = open(user_config_path, "w")
+
+    yaml.dump(default_data, user_config, default_flow_style=False)
 
 
 def get_config_path():
-    return config_path.resolve()
+    return user_config_path.resolve()
