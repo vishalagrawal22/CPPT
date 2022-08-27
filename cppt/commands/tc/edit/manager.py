dc999852b0f0758769366b01c7f8c2d3263db3dd
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-08-28 02:38:24 +0530

    Handle no testcase bug in tc commands

--- cppt/commands/tc/edit/manager.py
+++ cppt/commands/tc/edit/manager.py
@@ -22,50 +22,53 @@ def manage(filename, base_folder, tc_nos, editor):
     if tc_nos[0] == 0:
         tc_nos = task.get_tc_list()
 
-    testcases = []
-    tcs_data = task.get_tcs(tc_nos)
-    for tc_number, tc_data in zip(tc_nos, tcs_data):
-        if tc_data is None:
-            click.secho(f"TC #{tc_number} does not exist\n", fg="red")
-        else:
-            try:
-                testcase = {}
-                click.secho(f"Input #{tc_number}", fg="cyan")
-                testcase["input"] = click.edit(
-                    tc_data["input"],
-                    editor=editor,
-                )
+    if len(tc_nos) == 0:
+        click.secho("No testcases found!", fg="cyan")
+    else:
+        testcases = []
+        tcs_data = task.get_tcs(tc_nos)
+        for tc_number, tc_data in zip(tc_nos, tcs_data):
+            if tc_data is None:
+                click.secho(f"TC #{tc_number} does not exist\n", fg="red")
+            else:
+                try:
+                    testcase = {}
+                    click.secho(f"Input #{tc_number}", fg="cyan")
+                    testcase["input"] = click.edit(
+                        tc_data["input"],
+                        editor=editor,
+                    )
 
-                if testcase["input"] is None:
+                    if testcase["input"] is None:
+                        click.secho(
+                            "Input file closed without saving\n", fg="red", err=True
+                        )
+                        sys.exit(1)
+
+                    click.secho(testcase["input"] + "\n")
+
+                    click.secho(f"Output #{tc_number}", fg="cyan")
+                    testcase["output"] = click.edit(
+                        tc_data["output"],
+                        editor=editor,
+                    )
+                    if testcase["output"] is None:
+                        click.secho(
+                            "Output file closed without saving\n", fg="red", err=True
+                        )
+                        sys.exit(1)
+
+                    click.secho(testcase["output"] + "\n")
+
+                    testcases.append(testcase)
+
+                except click.UsageError:
                     click.secho(
-                        "Input file closed without saving\n", fg="red", err=True
+                        "Unable to open editor for testcase input (check config file for potential errors).\n",
+                        fg="red",
+                        err=True,
                     )
                     sys.exit(1)
 
-                click.secho(testcase["input"] + "\n")
-
-                click.secho(f"Output #{tc_number}", fg="cyan")
-                testcase["output"] = click.edit(
-                    tc_data["output"],
-                    editor=editor,
-                )
-                if testcase["output"] is None:
-                    click.secho(
-                        "Output file closed without saving\n", fg="red", err=True
-                    )
-                    sys.exit(1)
-
-                click.secho(testcase["output"] + "\n")
-
-                testcases.append(testcase)
-
-            except click.UsageError:
-                click.secho(
-                    "Unable to open editor for testcase input (check config file for potential errors).\n",
-                    fg="red",
-                    err=True,
-                )
-                sys.exit(1)
-
-    task.edit_tcs(tc_nos, testcases)
-    click.secho("Edited testcase files", fg="green")
+        task.edit_tcs(tc_nos, testcases)
+        click.secho("Edited testcase files", fg="green")
--- cppt/commands/tc/view/manager.py
+++ cppt/commands/tc/view/manager.py
@@ -21,13 +21,16 @@ def manage(filename, base_folder, tcs):
     if tcs[0] == 0:
         tcs = task.get_tc_list()
 
-    tcs_data = task.get_tcs(tcs)
-    for index, tc_data in enumerate(tcs_data):
-        if tc_data is None:
-            click.secho(f"TC #{tcs[index]} does not exist\n", fg="red")
-        else:
-            click.secho(f"Input #{tcs[index]}:", fg="cyan")
-            click.secho(tc_data["input"] + "\n")
+    if len(tcs) == 0:
+        click.secho("No testcases found!", fg="cyan")
+    else:
+        tcs_data = task.get_tcs(tcs)
+        for index, tc_data in enumerate(tcs_data):
+            if tc_data is None:
+                click.secho(f"TC #{tcs[index]} does not exist\n", fg="red")
+            else:
+                click.secho(f"Input #{tcs[index]}:", fg="cyan")
+                click.secho(tc_data["input"] + "\n")
 
-            click.secho(f"Output #{tcs[index]}: ", fg="cyan")
-            click.secho(tc_data["output"] + "\n")
+                click.secho(f"Output #{tcs[index]}: ", fg="cyan")
+                click.secho(tc_data["output"] + "\n")
