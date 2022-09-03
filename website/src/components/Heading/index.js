6c5cd763917508df8452bff3cc69b112952c4de9
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-09-03 14:06:56 +0530

    Remove console logs

--- website/src/components/Heading/index.js
+++ website/src/components/Heading/index.js
@@ -3,11 +3,7 @@ import React from "react";
 export default function Heading({ text }) {
   return (
     <span>
-      {console.log(text.split(" "))}
       {text.split(" ").map((word) => {
-        {
-          console.log(word[0], word.substr(1), word);
-        }
         return (
           <>
             <span style={{ color: "#080808" }}>{word[0]}</span>
